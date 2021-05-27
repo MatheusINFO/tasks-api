2f01a3b2bdf5913d0c22e07b65ccbcbf97830057
Matheus Andrade <matheus.add12@gmail.com>
2021-05-26 21:27:28 -0300

    feat: add QueryParams validation

--- src/tasks/dto/get-tasks-filter-dto.ts
+++ src/tasks/dto/get-tasks-filter-dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { Taskstatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(Taskstatus)
   status?: Taskstatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
